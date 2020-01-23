0602021683eacb42c609ba47324e887d322c4d51
kaur6869 <kaur6869@sheridancollege.ca>
2020-01-23 11:08:54 -0500

    first commit

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -4,8 +4,23 @@ package studentlist;
 
 public class Student
 {
+
+    /**
+     * @return the partime
+     */
+    public int getPartime() {
+        return partime;
+    }
+
+    /**
+     * @param partime the partime to set
+     */
+    public void setPartime(int partime) {
+        this.partime = partime;
+    }
     private String name;
     private int age;
+    private int partime;
 
    
     public String getName() {
