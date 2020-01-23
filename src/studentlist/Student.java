27105e9d645458fedfcbe0d38da8a15eb11269cc
kaur6869 <kaur6869@sheridancollege.ca>
2020-01-23 11:00:20 -0500

    first commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,32 @@
+
+package studentlist;
+
+
+public class Student
+{
+    private String name;
+    private int age;
+
+   
+    public String getName() {
+        return name;
+    }
+
+   
+    public void setName(String name) {
+        this.name = name;
+    }
+
+  
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
